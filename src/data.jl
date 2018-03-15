c6a52e665fe2a3761f232379057a2d92bdd4813f
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2018-03-15 11:14:01 -0700

    Small refactor (#19)
    * Split database loading and geolocation logic
    * add more tests and remove Compat from tests
    * Remove unexported unused function 'lookupgeoname' and 'mapcontains'
    * Add precompilation support
    * immutable -> struct
    
    
    Former-commit-id: 9f5fbe99f8fd3a68bf7b902a8c9468318c6ba2b0

---
+++ src/data.jl
@@ -0,0 +1,108 @@
+import Requests
+
+# Path to directory with data, can define GEOIP_DATADIR to override
+# the default (useful for testing with a smaller test set)
+const DATADIR = haskey(ENV, "GEOIP_DATADIR") ? 
+    ENV["GEOIP_DATADIR"] :
+    joinpath(dirname(@__FILE__), "..", "data")
+
+const MD5 = joinpath(DATADIR, ".md5") 
+const CITYMD5URL = "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City-CSV.zip.md5"
+const CITYDLURL = "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City-CSV.zip"
+
+const BLOCKCSV = "GeoLite2-City-Blocks-IPv4.csv"
+const CITYCSV = "GeoLite2-City-Locations-en.csv"
+
+const BLOCKCSVGZ = "$BLOCKCSV.gz"
+const CITYCSVGZ = "$CITYCSV.gz"
+
+dataloaded = false
+geodata = DataFrame()
+
+function readmd5()
+    if isfile(MD5)
+        return open(MD5) do f
+            strip(readline(f))
+        end
+    else
+        info("Failed to find checksum file, updating data...")
+        update()
+        readmd5()
+    end
+end
+
+function getmd5()
+    try
+        r = Requests.get(CITYMD5URL)
+        return string(r.data)
+    catch
+        error("Failed to download checksum file from MaxMind, check network connectivity")
+    end
+end
+
+updaterequired() = (readmd5() != getmd5())
+
+function dldata(md5::String)
+    r = try
+        Requests.get(CITYDLURL)
+    catch
+        error("Failed to download file from MaxMind, check network connectivity")
+    end
+
+    archive = ZipFile.Reader(IOBuffer(r.data))
+    dlcount = 0
+    for fn in archive.files
+        if contains(string(fn),BLOCKCSV)
+            gzopen(joinpath(DATADIR, BLOCKCSVGZ), "w") do f
+                write(f, read(fn))
+            end
+            dlcount += 1
+        elseif contains(string(fn),CITYCSV)
+            gzopen(joinpath(DATADIR, CITYCSVGZ), "w") do f
+                write(f, read(fn))
+            end
+            dlcount += 1
+        end
+    end
+
+    if dlcount == 2
+        open(MD5, "w") do f
+            write(f, md5)
+        end
+    else
+        error("Problem with download: only $dlcount of 2 files downloaded")
+    end
+end
+
+function update()
+    dldata(getmd5())
+    global dataloaded = false
+end
+
+function load()
+    blockfile = joinpath(DATADIR, BLOCKCSVGZ) 
+    locfile = joinpath(DATADIR, CITYCSVGZ)
+
+    blocks = DataFrame()
+    locs = DataFrame()
+    try
+         blocks = readtable(blockfile)
+         locs = readtable(locfile)
+    catch
+        error("Geolocation data cannot be read. Data directory may be corrupt...")
+    end
+    
+    # Clean up unneeded columns and map others to appropriate data structures
+    delete!(blocks, [:represented_country_geoname_id, :is_anonymous_proxy, :is_satellite_provider])
+    
+    blocks[:v4net] = map(x -> IPNets.IPv4Net(x), blocks[:network])
+    delete!(blocks, :network)
+    
+    blocks[:location] = map(Location, blocks[:longitude], blocks[:latitude])
+    delete!(blocks, [:longitude, :latitude])
+
+    alldata = join(blocks, locs, on=:geoname_id, kind=:inner)
+
+    global dataloaded = true
+    global geodata = sort(alldata, cols=[:v4net])
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,19 @@
 using Base.Test
-using Compat
 using GeoIP
 
+# Test known result
 ip1 = IPv4("1.2.3.4")
 geoip1 = geolocate(ip1; noupdate=false)
 @test geoip1[:country_iso_code] == "US"
 @test geoip1[:metro_code] == 819
-@test @compat(ceil(Int, geoip1[:location].x)) == -122
+@test ceil(Int, geoip1[:location].x) == -122
 
+# Test null results
 @test isempty(geolocate(ip"0.0.0.0"))
 @test isempty(geolocate(ip"127.0.0.1"))
+
+# Test array of ip's
+result = geolocate([ip"1.2.3.4", ip"8.8.8.8"])
+@test length(Set(result)) == 2
+@test !isempty(result[1])
+@test !isempty(result[2])  
