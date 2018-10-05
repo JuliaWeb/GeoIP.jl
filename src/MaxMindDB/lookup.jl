8327739abac2bd291e093162d4224985834b4674
Nathan Smith <nathan@nfsmith.ca>
2018-10-04 21:58:12 -0700

    [WIP] Binary search lookup
    Former-commit-id: 7205fb42ed5537aa8481fef177858e0369805bb4

---
+++ src/MaxMindDB/lookup.jl
@@ -0,0 +1,59 @@
+using Sockets
+
+
+function asbits(ip::IPAddr)
+    return UInt128(ip)
+end
+
+
+function firstnode(db::DB)
+    size = recordsize(db)
+    width = 2 * size
+    return db.buffer[1:(width - 1)]
+end
+
+
+function nextnode(db::DB, next)
+    size = recordsize(db)
+    width = 2 * size
+    return db.buffer[((next * width):(((next + 1) * width) - 1)]    
+end
+
+
+function left(node)
+    size = length(node)
+    r = isodd(size) ? UInt(node[(size >> 1) + 1] >> 4) : zero(UInt)
+    for byte in node[1:(size >> 1)]
+        r = (r << 8) | UInt(byte)
+    end
+    return r
+end
+
+
+function right(node)
+    size = length(node)
+    r = isodd(size) ? UInt(node[(size >> 1) + 1] & 0xF0) : zero(UInt)
+    for byte in node[((size >> 1) + 2):end]
+        r = (r << 8) | UInt(byte)
+    end
+    return r
+end
+
+
+function lookup(db::DB, ip::IPAddr)
+    bits = asbits(ip)
+    nodes = nodecount(db)
+    node = firstnode(db)
+    for i in 1:128
+        next = ((bits >> i) & 1) ? right(node) : left(node)
+        if next < nodes
+            node = nextnode(db, next)
+        elseif next == nodes
+            return nothing
+        else
+            break
+        end
+    end
+    ptr = (next - nodes) + treesize(db)
+    return decode(db, ptr)
+end
--- src/MaxMindDB/metadata.jl
+++ src/MaxMindDB/metadata.jl
@@ -2,6 +2,8 @@ export metadata
 
 const marker = b"\xab\xcd\xefMaxMind.com"
 
+# Cache for metadata calls
+const cache = Dict{DB, Any}()
 
 """
     metadata(db::DB) -> Dict
@@ -9,15 +11,36 @@ const marker = b"\xab\xcd\xefMaxMind.com"
 Returns a dictionary describing the database.
 """
 function metadata(db::DB)
+    if db in keys(cache)
+        return cache[db]
+    end
+
     b = db.buffer
     lb = length(b)
     lm = length(marker)
 
-    # Traverse buffer in reverse looking for marker
+    # Scan buffer in reverse looking for marker
     for i in (lb - lm):-1:1
         if b[i] == marker[1] && marker == b[i:(i + lm - 1)]
             return decode(db, i + lm)
         end
     end
     throw("Failed to find any metadata")
+end
+
+
+function nodecount(db::DB)
+    m = metadata(db)
+    return m["node_count"]
+end
+
+
+function recordsize(db::DB)
+    m = metadata(db)
+    return m["record_size"]    
+end
+
+
+function treesize(db::DB)
+    return (recordsize(db) / 4) * nodecount(db)
 end
